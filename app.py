85e52d9577d223b7abdba8915c2bd0958d0a7af9
Esteradias <ester_a_dias@hotmail.com>
2026-09-18 21:49:42 -0300

    up

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask 
- 
-app = Flask(__name__) 
- 
-@app.route("/") 
-def hello_world(): 
-   return "<p>Hello, World!</p>" 
+# Importando as dependências
+from flask import Flask, render_template
 
+# Inicializar variáveis e componentes
+
+# Nome do aplicativo (Site da Web) → global
+sitename = "Meu Flask"
+
+# Inicializa o plaicativo Flask (HTTP)
+app = Flask(__name__)
+
+
+# Rota da página inicial (rota raiz ou root)
+@app.route("/")
+def index():
+    return render_template(
+        "home.html",
+        sitename=sitename
+    )
+
+
+# Uma rota bem simples
+@app.route("/content")
+def content():
+    return render_template(
+        'content.html',
+        sitename=sitename
+    )
+# Uma rota bem simples
 @app.route("/about")
 def about():
-    output = "Sobre nós...."
-    return output
+    return render_template(
+        'about.html',
+        sitename=sitename
+    )
 
- 
+# Ativa o modo DEBUG e o main loop no localhost
 if __name__ == "__main__":
     app.run(debug=True)
-    
